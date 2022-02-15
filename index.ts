5b276a131724a0f1def9e7c99cd9e18a8aa16e4a
Loïc Vincent-Genod <37535451+VGLoic@users.noreply.github.com>
2022-02-15 14:12:08 +0100

    feat: move ethers to dependency (#2)
    * feat: add supportedNetworks key
    
    * feat: move ethers to dependency
    
    * feat: add isNetworkSupported utils

--- index.ts
+++ index.ts
@@ -26,6 +26,12 @@ function getNetworkConfigurationFactory(networkConfigurations: NetworkConfigurat
         return config;
     }
 }
+function isNetworkSupportedFactory(networkConfigurations: NetworkConfigurations): ((network: number) => boolean) {
+    return function isNetworkSupported(network: number) {
+        const config = networkConfigurations[network];
+        return Boolean(config);
+    }
+}
 
 const rainbowTokenConfigurations: NetworkConfigurations = {
     [Network.goerli]: {
@@ -41,6 +47,8 @@ const rainbowTokenConfigurations: NetworkConfigurations = {
 }
 
 const rainbowToken = {
+    supportedNetworks: Object.keys(rainbowTokenConfigurations),
+    isNetworkSupported: isNetworkSupportedFactory(rainbowTokenConfigurations),
     getNetworkConfiguration: getNetworkConfigurationFactory(rainbowTokenConfigurations),
 }
 
